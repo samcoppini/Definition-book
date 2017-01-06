1caa2ece6b8c35968af70885620cc9df7a592fe9
samcoppini <samcoppini@users.noreply.github.com>
2017-01-06 11:32:34 -0800

    Update for python 3, remove API key

--- definition_book.py
+++ definition_book.py
@@ -1,30 +1,32 @@
-from wordnik import *
 import codecs
 import random
+import os
+
+from wordnik import *
+
+def get_word(speech):
+	return words_api.getRandomWord(includePartOfSpeech=speech).word
 
 def make_sentence():
-	rand_sent  = "The "
-	rand_sent += get_sentence.getRandomWord(includePartOfSpeech="adjective").word
-	rand_sent += " "+get_sentence.getRandomWord(includePartOfSpeech="noun").word
-	rand_sent += " "+get_sentence.getRandomWord(includePartOfSpeech="verb-intransitive").word
-	rand_sent += " "+get_sentence.getRandomWord(includePartOfSpeech="preposition").word
-	rand_sent += " the "
-	rand_sent += get_sentence.getRandomWord(includePartOfSpeech="adjective").word
-	rand_sent += " "+get_sentence.getRandomWord(includePartOfSpeech="noun").word
-	return rand_sent
+	sentence_words = [
+		"The", get_word("adjective"), get_word("noun"),
+		get_word("verb-intransitive"), get_word("preposition"),
+		"the", get_word("adjective"), get_word("noun")
+	]
+	return " ".join(sentence_words)
 
 apiUrl = 'http://api.wordnik.com/v4'
-apiKey = '5a5dc229807e6f9ac7100019db5059207afa044bc21023efa'
+apiKey = os.getenv("API_KEY")
 client = swagger.ApiClient(apiKey, apiUrl)
 
-get_sentence = WordsApi.WordsApi(client)
-wordApi = WordApi.WordApi(client)
+words_api = WordsApi.WordsApi(client)
+word_api = WordApi.WordApi(client)
 title = make_sentence()
 novel = title.split()
 
 defined_words = ["you","are","unfamiliar","with","the","word","its","definition","is"]
 
-words_to_write = 50000
+words_to_write = 500
 
 while len(novel) < words_to_write:
 	random_index = random.randint(0,len(novel)-1)
@@ -34,17 +36,17 @@ while len(novel) < words_to_write:
 		if random_word[-1] == ".":
 			random_word = random_word[:-1]
 		try:
-			word_definition = wordApi.getDefinitions(random_word, limit=1)
+			word_definition = word_api.getDefinitions(random_word, limit=1)
 			if word_definition is not None:
 				word_definition = word_definition[0].text
 				to_add = "(If you are unfamiliar with the word '" + random_word + "', its definition is \"" + word_definition +"\")"
 				novel = novel[:random_index+1] + to_add.split() + novel[random_index+1:]
-				print str(len(novel)) + " words written"
+				print(str(len(novel)) + " words written")
 		except:
-			print "Took too long to respond"
+			print("Took too long to respond")
 novel = " ".join(novel)+"."
 file = codecs.open(title.capitalize()+".txt","w",encoding='utf-8')
 file.write(novel)
 file.close()
 
-end_program = raw_input("Novel written successfully!")
+print("Novel written successfully!")
